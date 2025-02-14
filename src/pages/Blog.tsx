2df5f024f2795e3c05bcc1b698dd165b32fffa38
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-14 14:12:34 +0200

    refactor Blog component to improve document head management; fix typo in meta description

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,22 +1,34 @@
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 
-const useDocumentHead = ({ title, describtion }) => {
+const useDocumentHead = ({
+    title,
+    description
+}: {
+    title: string,
+    description: string
+}) => {
     useEffect(() => {
         document.title = title
-        if (describtion) {
-            let metaDescribtion = document.querySelector('meta[name="describtion"]');
-            if(!metaDescribtion){
-                metaDescribtion = document.createElement('meta')
-                metaDescribtion.setAttribute('name', 'describtion')
-                document.head.appendChild
+        if (description) {
+            let metaDescription = document.querySelector('meta[name="description"]');
+            if (!metaDescription) {
+                metaDescription = document.createElement('meta')
+                metaDescription.setAttribute('name', 'description')
+                document.head.appendChild(metaDescription)
             }
+            metaDescription.setAttribute('content', description)
         }
-    }, [])
+    }, [title, description])
 }
 
 const Blog = () => {
     const { category, id } = useParams()
+
+    useDocumentHead({
+        title: 'My Page Title Deva',
+        description: 'My Page Description'
+    })
 
     return (
         <>
